b2da1459f8ee521fbf4230d72198bddafd79d124
Peter Chen <peter_chen@trimble.com>
2017-08-17 11:42:37 +0800

    Chain the pages together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
 import { AppComponent } from './app.component';
+import { CONST_ROUTING } from './app.routing';
+
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { WeatherComponent } from './weather/weather.component';
 import { CurrencyComponent } from './currency/currency.component';
 import { MovieComponent } from './movie/movie.component';
 
-import { CONST_ROUTING } from './app.routing';
 import { SharedService } from './_services/shared.service';
+import { AuthGuard } from './_guards/auth.guard';
+import { AuthenticationService } from './_services/authentication.service';
+import { UserService } from './_services/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
     MenuComponent,
     WeatherComponent,
     CurrencyComponent,
@@ -27,7 +40,16 @@ import { SharedService } from './_services/shared.service';
     HttpModule,
     CONST_ROUTING
   ],
-  providers: [SharedService],
+  providers: [
+    AuthGuard,
+    AuthenticationService,
+    UserService,
+    SharedService,
+    // providers used to create fake backend
+    fakeBackendProvider,
+    MockBackend,
+    BaseRequestOptions
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,18 @@
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './_guards/auth.guard';
 import { CurrencyComponent } from './currency/currency.component';
 import { WeatherComponent } from './weather/weather.component';
 import { MovieComponent } from './movie/movie.component';
 
 const MAINMENU_ROUTES: Routes = [
-  // full: makes sure the path is absolute path
-  { path: '', redirectTo: '/weather', pathMatch: 'full' },
-  { path: 'weather', component: WeatherComponent },
-  { path: 'movie', component: MovieComponent },
-  { path: 'currency', component: CurrencyComponent }
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'weather', component: WeatherComponent, canActivate: [AuthGuard] },
+  { path: 'movie', component: MovieComponent, canActivate: [AuthGuard] },
+  { path: 'currency', component: CurrencyComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' },
 ];
 
 export const CONST_ROUTING = RouterModule.forRoot(MAINMENU_ROUTES);
