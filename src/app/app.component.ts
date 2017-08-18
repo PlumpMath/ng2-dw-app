35fadb79de52263ed3b793a2906923db5aaf8fa4
Peter Chen <peter_chen@trimble.com>
2017-08-18 12:28:45 +0800

    add logo and update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles : ['.navbar-right { margin-right: 0px !important}']
 })
 export class AppComponent {
   title = 'a tutorial app';
